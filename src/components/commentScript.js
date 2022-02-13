0639dad0788a19b5a038d3b8017ddcf7c8680433
Gregorys Gonzalez <gregorysgonzalez@gmail.com>
2022-02-13 01:03:04 -0300

    arreglo fix en el script comment

--- src/components/commentScript.js
+++ src/components/commentScript.js
@@ -3,7 +3,7 @@ export default function setCommentScript() {
   // const theme = typeof window !== 'undefined' && localStorage.getItem(Theme) === DarkTheme ? 'github-dark' : 'github-light';
   commentScript.async = true;
   commentScript.src = 'https://utteranc.es/client.js';
-  commentScript.setAttribute('repo', process.env.commentBlog);
+  commentScript.setAttribute('repo', process.env.COMMENT_BLOG);
   commentScript.setAttribute('issue-term', 'pathname');
   commentScript.setAttribute('id', 'utterances');
   commentScript.setAttribute('theme', 'github-light');
